1314de4860d8532b6ff70968c70012f57391dfd3
David Rojas <drojascamaggi@gmail.com>
2013-02-15 14:05:33 -0300

    improve theme

--- public/javascripts/contactForm.js
+++ public/javascripts/contactForm.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
 		.find('[type="submit"]')
 		.click(function(e) {
 			e.preventDefault();
-			var email = contactForm.find('#email').val()
-			var subject = contactForm.find('#subject').val()
-			var message = contactForm.find('#message').val()
+			var email = contactForm.find('#email').val();
+			var subject = contactForm.find('#subject').val();
+			var message = contactForm.find('#message').val();
 
 			window.location.href = 'mailto:' +
 									'drojascamaggi@gmail.com' +
