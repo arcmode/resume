6eed608f44721bc4491017ed86dd6f158cae0599
David Rojas <drojascamaggi@gmail.com>
2013-02-23 16:33:01 -0300

    de-refactor chat and hotkeys

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -8,7 +8,7 @@ $('body').on('loaded', function(){
 		send = form.find('[type="submit"]'),
 		join = $('#join');
 
-	var name;
+	var name = undefined;
 
 	initSocket();
 
@@ -26,107 +26,177 @@ $('body').on('loaded', function(){
 		message.val('');
 	};
 
-	function register(name){
-		socket.emit('register', name);
-	};
-
-	function unregister(){
-		socket.emit('unregister');
-	};
-
 	function initSocket(){
-		socket.on('successful connection', function () {
+		$(document).bind('keydown', 'ctrl+i', function(){
 			join
-				.addClass('btn-info')
 				.removeClass('btn-danger disabled')
-				.html('Join');
-			initChat();
+				.addClass('btn-info')
+				.html('Join')
+				.unbind('click')
+				.bind('click', function(){
+					name = prompt('What\'s your name?');
+					if (name) {
+						socket.emit('register', name);
+					}
+				});
+		});
+		socket.on('successful connection', function () {
+			$('#server-status')
+				.removeClass('off')
+				.addClass('on');
 		});
 		socket.on('disconnect', function () {
+			$('#server-status').add($('#admin-status'))
+				.removeClass('on')
+				.addClass('off');
 			join
+				.addClass('disabled btn-danger')
 				.removeClass('btn-info')
-				.addClass('btn-danger disabled')
-				.html('Unavailable');
-			endChat();
+				.html('Unavailable')
+				.unbind('click');
+			message.add(send)
+				.attr('disabled', 'disabled');
+			send
+				.unbind('click');
+			name = undefined;
 		});
 		socket.on('message', function (data) {
-			appendMessage(data.from + ': ' + data.message);
+			appendMessage(data.message, data.from);
 		});
 		socket.on('rejected', function (data) {
-			joinChat(null, data.message);
+			name = prompt(data.message);
+			if (name) {
+				socket.emit('register', name);
+			}
 		});
-		socket.on('accepted', function (data) {
-			appendMessage(data.message + ' as ' + data.name);
-			enableChat();
+		socket.on('registered', function (data) {
+			if (data.adminIsConnected || data.isAdmin) {
+				$('#admin-status')
+					.removeClass('off')
+					.addClass('on');
+				appendMessage(data.message + ' as ' + data.name);
+				join
+					.removeClass('btn-info disabled')
+					.addClass('btn-danger')
+					.unbind('click')
+					.html('Leave')
+					.bind('click', function(){
+						socket.emit('unregister');
+					});
+				message
+					.add(send)
+					.removeAttr('disabled');
+				send
+					.unbind('click')
+					.bind('click', sendMessage);
+			} else {
+				appendMessage(data.message);
+			}
 		});
 		socket.on('unregistered', function(data) {
 			appendMessage(data.name + ', ' + data.message);
-			disableChat();
+			join
+				.addClass('btn-info')
+				.removeClass('btn-danger')
+				.unbind('click')
+				.html('Join')
+				.unbind('click')
+				.bind('click', function(){
+					name = prompt('What\'s your name?');
+					if (name) {
+						socket.emit('register', name);
+					}
+				});
+			message.add(send)
+				.attr('disabled', 'disabled');
+			send
+				.unbind('click');
+			name = undefined;
 		});
 		socket.on('restricted name', function(data) {
 			appendMessage(data.message);
 		});
+		socket.on('admin leaved', function () {
+			$('#admin-status')
+				.removeClass('on')
+				.addClass('off');
+			join
+				.addClass('disabled btn-danger')
+				.removeClass('btn-info')
+				.html('Unavailable')
+				.unbind('click');
+			message.add(send)
+				.attr('disabled', 'disabled');
+			send
+				.unbind('click');
+		});
+		socket.on('admin is joined', function () {
+			$('#admin-status')
+				.removeClass('off')
+				.addClass('on');
+			join
+				.addClass('btn-info')
+				.removeClass('disabled btn-danger')
+				.html('Join')
+				.unbind('click')
+				.bind('click', function(){
+					name = prompt('What\'s your name?');
+					if (name) {
+						socket.emit('register', name);
+					}
+				});
+		});
+		socket.on('admin just joined', function () {
+			$('#admin-status')
+				.removeClass('off')
+				.addClass('on');
+			if (name) {
+				join
+					.removeClass('disabled')
+					.html('Leave')
+					.unbind('click')
+					.bind('click', function(){
+						socket.emit('unregister');
+					});
+				message.add(send)
+					.removeAttr('disabled');
+				send
+					.unbind('click')
+					.bind('click', sendMessage);
+			} else {
+				join
+					.addClass('btn-info')
+					.removeClass('disabled btn-danger')
+					.html('Join')
+					.unbind('click')
+					.bind('click', function(){
+						name = prompt('What\'s your name?');
+						if (name) {
+							socket.emit('register', name);
+						}
+					});
+			}
+		});
 	};
 
-	function appendMessage(message) {
-		var td = $('<td/>', { 'html': message }),
+	function appendMessage(message, from) {
+		var td = $('<td/>', { 
+					'class': 'message',
+					'html': message
+					}),
 			tr = $('<tr/>', { 'html': td })
-				.appendTo(messages.find('tbody'));
-	};
-
-	function endChat(){
-		join.unbind('click');
-		chat
-			.find('input, button')
-			.not('#join')
-			.attr('disabled', 'disabled');
-	};
-
-	function initChat(){
-		join
-			.bind('click', joinChat)
-			.unbind('click', leaveChat);
-	};
-
-	function enableChat(){
-		chat
-			.find('[disabled]')
-			.removeAttr('disabled');
-		join
-			.removeClass('btn-info')
-			.addClass('btn-danger')
-			.html('Leave')
-			.unbind('click', joinChat)
-			.bind('click', leaveChat);
-		send.bind('click', sendMessage);
-	};
-
-	function disableChat(){
-		chat
-			.find('input, button')
-			.not('#join')
-			.attr('disabled', 'disabled');
-		join
-			.removeClass('btn-danget')
-			.addClass('btn-info')
-			.html('Join')
-			.unbind('click', leaveChat)
-			.bind('click', joinChat);
-		send.unbind('click', sendMessage);
-	};
-
-	function leaveChat(event){
-		event.preventDefault();
-		unregister();
-	};
-
-	function joinChat(event, text){
-		if (event) {
-			event.preventDefault();
-		}
-		name = prompt(text || 'What\'s your name?');
-		if (name) {
-			register(name);
+					.appendTo(messages.find('tbody'));
+		if (from) {
+			from = $('<td/>', { 
+					'class': 'from',
+					'html': from
+					 })
+					.prependTo(tr);
+		} else {
+			var fake = $('<td/>', { 
+					'class': 'from'
+					 })
+					.prependTo(tr);
 		}
 	};
 });
